8d21b5573f9c911c56ef8be5acb94677411ba412
ahstein3521 <adamhs3521@gmail.com>
2016-12-19 20:01:58 -0500

    fixed typo

--- server/models/players/_goalie.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-var Player = require("./main");
-
-var GoalieStatsSchema = require("./goalieStatSchema")
-
-var goalie = Player.discriminator("Goalie", GoalieStatsSchema);
-
-module.exports = goalie;
